0aaecc3e1ed67b037cda0ba59bb51b5a868b1526
prana.wijaya <prana.2015013004@univ.atmajaya.ac.id>
2020-08-04 17:30:42 +0700

    add pwa to home screen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <input type="text" name="username" v-model="username"/> <br>
-    <button @click="getUserData"> Search </button>
+    <button @click="getUserData"> Search</button>
 
     <div v-if="user !== null" class="profile">
       <img v-bind:src="user.avatar_url" width="200" height="200"> <br>
@@ -10,66 +10,89 @@
       <label>Profile name : {{ user.name }}</label> <br>
       <label>Profile bio : {{ user.bio }}</label>
     </div>
+    <button v-if="deferredPrompt" @onClick="promptInstall">
+      Add to home screen
+    </button>
   </div>
 </template>
 
 <script>
-  import GithubService from '../services/GithubService'
-  export default {
-    name: 'hello',
-    data () {
-      return {
-        msg: 'VueJS PWA consuming Github API',
-        username: '',
-        user: null
+import GithubService from '../services/GithubService';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'VueJS PWA consuming Github API',
+      username: '',
+      user: null
+    }
+  },
+  methods: {
+    async getUserData() {
+      this.user = null;
+      await GithubService.searchUser({
+        username: this.username
+      }).then(response => {
+        this.user = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+      if (this.user !== null) {
+        console.log(this.user);
       }
     },
-    methods: {
-      async getUserData() {
-        this.user = null;
-        await GithubService.searchUser({
-          username: this.username
-        }).then(response => {
-          this.user = response.data
-        }).catch(error => {
-          console.log(error)
-        })
-        if(this.user !== null) {
-          console.log(this.user);
+    promptInstall() {
+      // Show the prompt:
+      this.deferredPrompt.prompt();
+
+      // Wait for the user to respond to the prompt:
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt");
+        } else {
+          console.log("User dismissed the install prompt");
         }
-      }
+
+        this.deferredPrompt = null;
+      });
     }
   }
+}
 </script>
 
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-  input {
-    width: 40%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-  }
-  button {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-  }
-  .profile {
-    padding: 16px;
-  }
-  label {
-    font-size: 20px;
-  }
+h1, h2 {
+  font-weight: normal;
+}
+
+input {
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.profile {
+  padding: 16px;
+}
+
+label {
+  font-size: 20px;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VuePwaInstallPlugin from "vue-pwa-install";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VuePwaInstallPlugin);
 
 new Vue({
   render: h => h(App),
